Test BCE loss. Fix CV metric calculation

src/cgan/counterfactual_lungs_cgan.py
@@ -55,9 +55,7 @@ class CounterfactualLungsCGAN(nn.Module):
         self.classifier_f.eval()
 
         # Loss functions
-        self.adversarial_loss = torch.nn.MSELoss()
-        self.KL_loss = torch.nn.KLDivLoss()
-        # self.adversarial_loss = torch.nn.BCEWithLogitsLoss()
+        self.adversarial_loss = torch.nn.BCEWithLogitsLoss() if opt.get('adv_loss', 'mse') == 'bce' else torch.nn.MSELoss()
         self.lambda_adv = opt.get('lambda_adv', 1.0)
         self.lambda_kl = opt.get('lambda_kl', 1.0)
         self.lambda_rec = opt.get('lambda_rec', 1.0)

src/trainer.py
@@ -166,7 +166,7 @@ class Trainer:
 
         posterior_true, posterior_pred = np.array(posterior_true), np.array(posterior_pred)
         # Counterfactual Validity score 
-        cv_score = np.mean((posterior_true - posterior_pred) > tau)
+        cv_score = np.mean(np.abs(posterior_true - posterior_pred) > tau)
         self.logger.info(f'CV(X, Xc) = {cv_score:.3f} (τ={tau}, num_samples={len(posterior_true)})')
 
         with open(out_dir / 'probs.txt', 'w') as fid: